3db32c99d2b11f9d06a0d0ac090336ca35e955df
cnallam <130782580+cnallam@users.noreply.github.com>
2023-05-04 11:49:26 -0700

    Update welcome-message.sh

--- .devcontainer/advanced-integration/welcome-message.sh
+++ .devcontainer/advanced-integration/welcome-message.sh
@@ -3,14 +3,14 @@
 set -e
 
 WELCOME_MESSAGE="
-👋 Welcome to the \"Checkout Advanced Integration Example\"
+👋 Welcome to the \"Advanced Checkout Integration Example\"
 
 🛠️  Your environment is fully setup with all the required software.
 
 🚀 Once you rename the \".env.example\" file to \".env\" and update \"CLIENT_ID\" and \"APP_SECRET\", the checkout page will automatically open in the browser after the server is restarted."
 
 ALTERNATE_WELCOME_MESSAGE="
-👋 Welcome to the \"Checkout Advanced Integration Example\"
+👋 Welcome to the \"Advanced Checkout Integration Example\"
 
 🛠️  Your environment is fully setup with all the required software.
 
