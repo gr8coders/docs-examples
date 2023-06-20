177579bb551795c7ee7f51456b62ebe78fa6bed8
gr8coders <94279589+gr8coders@users.noreply.github.com>
2023-06-20 14:45:57 -0700

    Update welcome-message.sh

--- .devcontainer/advanced-integration/welcome-message.sh
+++ .devcontainer/advanced-integration/welcome-message.sh
@@ -20,4 +20,6 @@ if [ -n "$CLIENT_ID" ] && [ -n "$APP_SECRET" ]; then
     WELCOME_MESSAGE="${ALTERNATE_WELCOME_MESSAGE}"
 fi
 
-sudo bash -c "echo \"${WELCOME_MESSAGE}\" > /usr/local/etc/vscode-dev-containers/first-run-notice.txt"
+# sudo bash -c "echo \"${WELCOME_MESSAGE}\" > /usr/local/etc/vscode-dev-containers/first-run-notice.txt"
+mkdir -p $HOME/.config/vscode-dev-containers/ && touch $HOME/.config/vscode-dev-containers/first-run-notice.txt
+bash -c "echo \"${WELCOME_MESSAGE}\" > $HOME/.config/vscode-dev-containers/first-run-notice.txt"
